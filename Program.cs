4912ef9b31ee36c760e346d9106a94cdd77d4157
Jan Dobš <Jenda2003@gmail.com>
2020-07-10 20:33:42 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TF2_Log_IP_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
